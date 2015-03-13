b82c5861de703e302c1aa2e7a56558af6e994c97
faderskd <daniel.faderski@gmail.com>
2015-03-13 15:19:32 +0100

    prawa wstepne do poprawki

--- api/permissions.py
+++ api/permissions.py
@@ -1,23 +1,73 @@
 from rest_framework import permissions
 
-class IsAccountOwnerOrReadOnly(permissions.BasePermission):
+
+class IsAccountOwnerOrIsAdminOrReadOnly(permissions.BasePermission):
     """
     Odpowiada za prawa do konta, jeżeli user jest właścicielem i jest zalogowany
     może edytować, w przeciwnym wypadku może tylko przeglądać
     """
 
+    def has_permission(self, request, view):
+        if request.method == 'POST':
+            return False
+        return True
+
     def has_object_permission(self, request, view, obj):
+        if request.method == 'DELETE' and request.user.is_staff:
+            return True
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj == request.user
 
-class IsOwnerOrReadOnly(permissions.BasePermission):
+
+class IsOwnerOrIsAdminOrReadOnly(permissions.BasePermission):
     """
-    Odpowiada za prawa do profilu, jeżeli user jest właścicielem i jest zalogowany
-    może edytować, w przeciwnym wypadku może tylko przeglądać
+    Odpowiada za prawa do obiektu, jeżeli user jest właścicielem i jest zalogowany
+    może edytować w przeciwnym wypadku może tylko przeglądać
+    Admin może usuwać
     """
 
-    def has_object_permission(self, request, view, obj):
+    def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
+        return request.user.is_active
+
+    def has_object_permission(self, request, view, obj):
+        if request.method == 'DELETE' and request.user.is_staff:
+            return True
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.user == request.user
+
+
+class IsAdminOrReadOnly(permissions.BasePermission):
+    """
+    Wszyscy poza adminem mają prawa tylko do odczytu
+    """
+
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user.is_staff
+
+
+class IsQuestionAuthorOrHasAnsweredOrIsReadOnly(permissions.BasePermission):
+    """
+
+    """
+
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user.is_active
+
+    def has_object_permission(self, request, view, obj):
+        if request in permissions.SAFE_METHODS:
+            return True
+        if request.method == 'DELETE' and request.user.is_staff:
+            return True
+        if request.method == 'POST':
+            has_answered = obj.question.answer_set.filter(user=request.user)
+            if has_answered:
+                return False
         return obj.user == request.user
--- api/views.py
+++ api/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from rest_framework import permissions
 from rest_framework import viewsets
 from api.serializers import UserSerializer, UserProfileSerializer
-from api.permissions import IsAccountOwnerOrReadOnly,IsOwnerOrReadOnly
+from api.permissions import IsAccountOwnerOrIsAdminOrReadOnly,IsOwnerOrIsAdminOrReadOnly
 from api.models import UserProfile
 from rest_framework import mixins
 
@@ -12,7 +12,7 @@ class UserViewSet(viewsets.ModelViewSet):
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,IsAccountOwnerOrReadOnly,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,IsAccountOwnerOrIsAdminOrReadOnly,)
 
 
 class UserProfileViewSet(viewsets.GenericViewSet,mixins.RetrieveModelMixin,mixins.UpdateModelMixin,mixins.ListModelMixin):
@@ -21,7 +21,7 @@ class UserProfileViewSet(viewsets.GenericViewSet,mixins.RetrieveModelMixin,mixin
     """
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrIsAdminOrReadOnly,)
 
 
 
