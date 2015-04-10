521c525a082aacd9811d66ad9054d818106f042c
Bartłomiej Bęczkowski <beczkowb@gmail.com>
2015-04-10 13:40:18 +0200

    Małe poprawki w modelach, dodane verbosy

--- api/models.py
+++ api/models.py
@@ -9,8 +9,11 @@ class Category(models.Model):
     """
     Kategoria tematyczna
     """
-    name = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100, unique=True, verbose_name=_("name"))
     slug = models.SlugField(unique=True)
+
+    class Meta:
+        verbose_name_plural = _("categories")
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
@@ -24,24 +27,23 @@ class Question(models.Model):
     """
     Pytanie
     """
-    title = models.CharField(max_length=300, unique=True, blank=False)
-    content = models.TextField(blank=False)
-    category = models.ForeignKey(Category, blank=False, null=False, related_name='questions')
-    user = models.ForeignKey(User, blank=False, null=False, related_name='questions')
-    set_date = models.DateTimeField(auto_now_add=True)
-    last_activity = models.DateTimeField(auto_now_add=True)
-    solved = models.BooleanField(default=False)
-    views = models.PositiveIntegerField(default=0)
-    likes = models.PositiveIntegerField(default=0)
-    dislikes = models.PositiveIntegerField(default=0)
-    favorites = models.PositiveIntegerField(default=0)
-
+    title = models.CharField(max_length=300, unique=True, verbose_name=_("question"))
+    content = models.TextField(verbose_name=_("content"))
+    category = models.ForeignKey(Category, related_name='questions', verbose_name=_("category"))
+    user = models.ForeignKey(User, related_name='questions', verbose_name=_("user"))
+    set_date = models.DateTimeField(auto_now_add=True, verbose_name=_("set date"))
+    last_activity = models.DateTimeField(auto_now=True, verbose_name=_("last activity"))
+    solved = models.BooleanField(default=False, verbose_name=_("solved"))
+    views = models.PositiveIntegerField(default=0, verbose_name=_("views"))
+    likes = models.PositiveIntegerField(default=0, verbose_name=_("likes"))
+    dislikes = models.PositiveIntegerField(default=0, verbose_name=_("dislikes"))
+    favorites = models.PositiveIntegerField(default=0, verbose_name=_("favorites"))
 
     def edited(self):
         return (self.last_activity - self.set_date).seconds > 60
 
     def likebalance(self):
-        return (self.likes - self.dislikes)
+        return self.likes - self.dislikes
 
     def __str__(self):
         return _('%s' % self.title)
@@ -52,14 +54,14 @@ class Answer(models.Model):
     Odpowiedź do pytania, user nie może odpowiadać więcej niż raz na dane pytanie
     może tylko edytować
     """
-    content = models.TextField(blank=False)
-    question = models.ForeignKey(Question, blank=True, null=True, related_name='answers')
-    user = models.ForeignKey(User, blank=False, null=False, related_name='answers')
-    set_date = models.DateTimeField(auto_now_add=True)
-    last_activity = models.DateTimeField(auto_now_add=True)
-    likes = models.PositiveIntegerField(default=0)
-    dislikes = models.PositiveIntegerField(default=0)
-    solved = models.BooleanField(default=False)
+    content = models.TextField()
+    question = models.ForeignKey(Question, blank=True, null=True, related_name='answers', verbose_name=_("content"))
+    user = models.ForeignKey(User, related_name='answers', verbose_name=_("user"))
+    set_date = models.DateTimeField(auto_now_add=True, verbose_name=_("set date"))
+    last_activity = models.DateTimeField(auto_now=True, verbose_name=_("last activity"))
+    likes = models.PositiveIntegerField(default=0, verbose_name=_("likes"))
+    dislikes = models.PositiveIntegerField(default=0, verbose_name=_("dislikes"))
+    solved = models.BooleanField(default=False, verbose_name=_("solved"))
 
     def edited(self):
         return (self.last_activity - self.set_date).seconds > 60
@@ -68,15 +70,16 @@ class Answer(models.Model):
         return self.likes - self.dislikes
 
     def __str__(self):
-        return '%s' % (self.question.title)
+        return '%s' % (self.question.title, )
 
 
 class Tag(models.Model):
     """
     Tag - zgrupowanie podobnych pytań
     """
-    questions = models.ManyToManyField(Question, blank=True, null=True, related_name='tags')
-    name = models.CharField(max_length=100, unique=True)
+    questions = models.ManyToManyField(Question, blank=True, null=True, related_name='tags',
+                                       verbose_name=_("questions"))
+    name = models.CharField(max_length=100, unique=True, verbose_name=_("name"))
     slug = models.SlugField(unique=True)
 
     def save(self, *args, **kwargs):
@@ -92,14 +95,14 @@ class Comment(models.Model):
     Komentarz do pytania lub odpowiedzi, można go dodawać dla pytania lub odpowiedzi ale tylko
     dla jednego z nich
     """
-    content = models.TextField(blank=False)
-    user = models.ForeignKey(User, blank=False, null=False, related_name='comments')
-    set_date = models.DateTimeField(auto_now_add=True)
-    last_activity = models.DateTimeField(auto_now_add=True)
-    likes = models.PositiveIntegerField(default=0)
-    dislikes = models.PositiveIntegerField(default=0)
-    question = models.ForeignKey(Question, blank=True, null=True, related_name='comments')
-    answer = models.ForeignKey(Answer, null=True, blank=True, related_name='comments')
+    content = models.TextField(verbose_name=_("content"))
+    user = models.ForeignKey(User, related_name='comments', verbose_name=_("user"))
+    set_date = models.DateTimeField(auto_now_add=True, verbose_name=_("set date"))
+    last_activity = models.DateTimeField(auto_now=True, verbose_name=_("last activity"))
+    likes = models.PositiveIntegerField(default=0, verbose_name=_("likes"))
+    dislikes = models.PositiveIntegerField(default=0, verbose_name=_("dislikes"))
+    question = models.ForeignKey(Question, blank=True, null=True, related_name='comments', verbose_name=_("question"))
+    answer = models.ForeignKey(Answer, null=True, blank=True, related_name='comments', verbose_name=_("answer"))
 
     def clean(self):
         if self.question and self.answer:
@@ -114,17 +117,17 @@ class Comment(models.Model):
         return self.likes - self.dislikes
 
     def __str__(self):
-        return '%s' % (self.content)
+        return '%s' % (self.content, )
 
 
 class UserProfile(models.Model):
     """
     Profil użytkownika tworzony równocześnie z użytkownikiem
     """
-    user = models.OneToOneField(User, null=False)
+    user = models.OneToOneField(User)
     picture = models.ImageField(upload_to='profile_images', default='profile_images/blank-profile.jpg', null=True,
-                                blank=True)
-    created = models.DateTimeField(auto_now_add=True)
+                                blank=True, verbose_name=_("picture"))
+    created = models.DateTimeField(auto_now_add=True, verbose_name=_("created"))
 
     def __str__(self):
         return _('%s' % self.user.username)
@@ -134,15 +137,21 @@ class UserPreferences(models.Model):
     """
     Preferencje użytkownika zawierają listy lubianych i nielubianych pytań, odpowiedzi, i komentarzy
     """
-    user = models.OneToOneField(User, null=False)
-    favorite_questions = models.ManyToManyField(Question, null=True, blank=True, related_name="favorite_questions")
-    liked_questions = models.ManyToManyField(Question, null=True, blank=True, related_name="liked_questions")
-    disliked_questions = models.ManyToManyField(Question, null=True, blank=True, related_name="disliked_questions")
-    liked_answers = models.ManyToManyField(Answer, null=True, blank=True, related_name='liked_answers')
-    disliked_answers = models.ManyToManyField(Answer, null=True, blank=True, related_name='disliked_answers')
-    liked_comments = models.ManyToManyField(Comment, null=True, blank=True, related_name='liked_comments')
+    user = models.OneToOneField(User)
+    favorite_questions = models.ManyToManyField(Question, null=True, blank=True, related_name="favorite_questions",
+                                                verbose_name=_("favorite questions"))
+    liked_questions = models.ManyToManyField(Question, null=True, blank=True, related_name="liked_questions",
+                                             verbose_name=_("liked questions"))
+    disliked_questions = models.ManyToManyField(Question, null=True, blank=True, related_name="disliked_questions",
+                                                verbose_name=_("disliked questions"))
+    liked_answers = models.ManyToManyField(Answer, null=True, blank=True, related_name='liked_answers',
+                                           verbose_name=_("liked answers"))
+    disliked_answers = models.ManyToManyField(Answer, null=True, blank=True, related_name='disliked_answers',
+                                              verbose_name=_("disliked answers"))
+    liked_comments = models.ManyToManyField(Comment, null=True, blank=True, related_name='liked_comments',
+                                            verbose_name=_("liked comments"))
     disliked_comments = models.ManyToManyField(Comment, null=True, blank=True,
-                                               related_name='disliked_comments')
+                                               related_name='disliked_comments', verbose_name=_("disliked comments"))
 
     def __str__(self):
         return '%s' % self.user.username
