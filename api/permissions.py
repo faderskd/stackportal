e353d4a8883a85e16cfc15ccc13df5a59b252f58
rafalbasiak <rafalbasiak93@gmail.com>
2015-04-13 21:43:52 +0200

    Edycja permission IsOwnerOrIsAdminOrReadOnly - dodanie adminowi możliwości edycji nie swoich pytań

--- api/permissions.py
+++ api/permissions.py
@@ -36,7 +36,7 @@ class IsOwnerOrIsAdminOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        if request.method == 'DELETE' and request.user.is_staff:
+        if request.method == 'DELETE' or request.method == 'PUT' and request.user.is_staff:
             return True
         return obj.user == request.user
 
