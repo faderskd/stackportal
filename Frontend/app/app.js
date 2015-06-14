2c4fff4bad51904df646095cb421143c5e876964
rafalboguski <Ab123456>
2015-06-14 17:44:08 +0200

    Logowanie i Rejestracja w nav

--- Frontend/app/app.js
+++ Frontend/app/app.js
@@ -159,3 +159,80 @@ $(function () {
         alert('clicked!');
     });
 });
+
+
+
+angular.module('myApp.Login', ['ngRoute', 'ngCookies'])
+
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/Login', {
+            templateUrl: 'Login/Login.html',
+            controller: 'LoginCtrl'
+        });
+    }])
+    .controller('LoginCtrl', function ($scope, $location, $rootScope, $cookies) {
+        $scope.model = {'username':'','password':''};
+        $scope.complete = false;
+        $scope.login = function(){
+            $scope.errors = [];
+            //Validate.form_validation(formData,$scope.errors);
+            //if(!formData.$invalid){
+            $rootScope.GlobalService.Login($scope.model.username, $scope.model.password)
+                .then(function(response){
+                    $rootScope.GlobalService.GetUsers().then(function(response)
+                    {
+                        var i;
+                        for (i = 0; i < response.data.length; i++)
+                            if (response.data[i].username == $scope.model.username)
+                            {
+                                $cookies.put("username", response.data[i].username);
+                                $cookies.put("userId", response.data[i].id);
+                            }
+                    });
+                    $location.path("/");
+                },function(response){
+                    // error case
+                    $scope.errors = response.data;
+                });
+        }
+    });
+
+
+angular.module('myApp.Register', ['ngRoute'])
+
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/Register', {
+            templateUrl: 'Register/Register.html',
+            controller: 'RegisterCtrl'
+        });
+    }])
+    .controller('RegisterCtrl', function ($scope, $location, $rootScope) {
+        $scope.model = {'username': '', 'password': '', 'password2': '', 'Email': ''};
+        $scope.register = function () {
+            $scope.errors = [];
+            $scope.errors.username = [];
+            $scope.errors.email = [];
+            $scope.errors.password = [];
+            if ($scope.model.password.length < 6) {
+                $scope.errors.password.push('Hasło jest za krótkie');
+                return;
+            }
+            if ($scope.model.password != $scope.model.password2) {
+                $scope.errors.password.push('Hasła są różne');
+                return;
+            }
+
+            $rootScope.GlobalService.Register($scope.model.username, $scope.model.Email, $scope.model.password)
+                .then(function (response) {
+                    $scope.model.username ="";
+                    $scope.model.Email = "";
+                    $scope.model.password = "";
+                    $scope.model.password2 = "";
+                    alert("Dodano nowe konto");
+                    $location.path("/Questions");
+                }, function (response) {
+                    // error case
+                    $scope.errors = response.data;
+                });
+        }
+    });
